ef3d6b86a0be556722ac0b07c0d5993fef9eccff
Alexandr <hookahdelivery31@yandex.ru>
2024-01-22 23:53:49 +0300

    Update

---
+++ handlers/user_private.py
@@ -0,0 +1,17 @@
+from aiogram import types, Router
+from aiogram.filters import CommandStart, Command
+
+user_private_router = Router()
+
+@user_private_router.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer('Привет, я виртуальный помощник')
+
+
+@user_private_router.message(Command('menu'))
+async def menu_cmd(message: types.Message):
+    await message.answer("Вот меню:")
+
+@user_private_router.message(Command('about'))
+async def menu_cmd_about(message: types.Message):
+    await message.answer("О нас:")
