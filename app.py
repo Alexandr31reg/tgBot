dacf7af64d2b743476cab92d62f272da0b0e4ece
Alexandr <hookahdelivery31@yandex.ru>
2024-01-23 23:39:52 +0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from common.bot_cmd_list import private
 from handlers.user_private import user_private_router
+from handlers.user_group import user_group_router
 import asyncio
 import os
 
@@ -15,10 +17,13 @@ bot = Bot(token=os.getenv('TOKEN_API'))
 dp = Dispatcher()
 
 dp.include_router(user_private_router)
+dp.include_router(user_group_router)
 
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
+    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats)
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATE)
 
 
